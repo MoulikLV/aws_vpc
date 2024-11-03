2be93553af542bebfafecca4f9943df19f96ce0c
MoulikLV <155300423+MoulikLV@users.noreply.github.com>
2024-11-03 17:33:01 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -11,6 +11,6 @@ resource "aws_subnet" "subnet" {
   vpc_id = aws_vpc.vpc1.id
   cidr_block = "192.168.1.0/24"
   tags = {
-    Name = "Terra -jenkinsubnet"
+    Name = "Terra -jenkin-subnet"
   }
 }
