abef982abe0e1a6d30a50764c646edf160394cd8
MoulikLV <155300423+MoulikLV@users.noreply.github.com>
2024-11-03 16:38:54 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,6 +4,16 @@ provider "aws" {
   secret_key = var.aws_secret_access_key    # Reference the Secret Key variable
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "terraformbackend231"        # Replace with your S3 bucket name
+    key            = "terraform.tfstate"             # The path within the bucket to store the state file
+    region         = "us-east-1"                   # Replace with your desired AWS region
+      
+    encrypt        = true                           # Optional: Enable encryption for the state file
+  }
+}
+
 
 
 
