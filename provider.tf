0efb6e2d1c6ecffad32f48277931fb93026bb971
MoulikLV <155300423+MoulikLV@users.noreply.github.com>
2024-11-12 19:50:05 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region     = "us-east-1"  # Specify your desired AWS region
-  access_key = var.aws_access_key_id       # Reference the Access Key variable
-  secret_key = var.aws_secret_access_key    # Reference the Secret Key variable
+  access_key = "AKIAQ3EGSVWSFXYHAFDZ"
+  secret_key = "9FpQ52sLpNwNJtTWH+PfpjI3ZNZBTVmGs8cFoAMB"
+  region = "us-east-1"
 }
 
 terraform {
   backend "s3" {
-    bucket         = "terraformbackend231"        # Replace with your S3 bucket name
+    bucket         = "terraformstatefilebucket123"        # Replace with your S3 bucket name
     key            = "terraform.tfstate"             # The path within the bucket to store the state file
     region         = "us-east-1"                   # Replace with your desired AWS region
       
